5360c8c52289a624e9f742de08628b3770ecb7df
Siddiqua <93445702+ayesiddiqua@users.noreply.github.com>
2021-11-03 19:55:12 +0530

    Add files via upload
    lesson-1

---
+++ python-basic.py
@@ -0,0 +1,52 @@
+#printing hello world
+print('hello world')
+#simple operations
+#addition
+print(2+2)
+#subtraction
+print(2-2)
+print(4-2+1)
+#multiplication
+print(2*2)
+print(2*(2-1))
+#division
+print(2/2)
+print(3/4)
+#exponentiation
+print(2**3)
+#you can rise a number to multile powers
+print(2**3**2)
+#you can also use floats in exponentiation
+print(4**(1/2))
+#quotient
+print(25//6)
+#remainder
+print(25%6)
+#all numerical operators can also be used with floats
+print(1.25%0.5)
+#strings
+print("python for everybody!!")
+#uses of backslash
+#sometimes a single quote in a single quoted string or a double quote in a double quoted string would confuse python and break the code
+#here's what you need to do, escape the quotes using a backslash
+print('jon\'s mother:he\'s not an angel.he\'s very naughty boy!')
+#newlines
+#\n can be used in strings to create a multiple lines
+print('red\ngreen\nyellow')
+#\t is used for tab representing certain whitespace characters
+print('red\tblue\tyellow')
+print('red', 'blue', 'yellow')
+"""You can specify multiple strings with the print statement.
+Just separate them out with a comma ',', and they will be printed with a space in between:"""
+'It\'s wed-nes-day'
+print(""" hello world
+          this is a
+          multi line text""")
+"""concatenation
+   maths works with words as well
+   & strings containing numbers are stil added as strings rather than integers""" 
+print("git"+"hub")
+print("1"+"2")
+#you can also multiply string by an integer
+print("he"*3)
+print(2*'7')
